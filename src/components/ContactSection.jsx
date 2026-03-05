0ec11309ddeb3749686797b20c2dc5cac7aedf82
Ananya Singh <ananya18072006@gmail.com>
2026-03-05 17:26:33 +0530

    improved

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -2,12 +2,7 @@ import { Instagram, Linkedin, Mail, MapPin, Phone, Send, Twitter } from "lucide-
 
 
 export const ContactSection = () => {
-    const handleSubmit =(e) => {
-        e.preventDefault()
-        setTimeout(()=> {
-
-        }, 1500)
-    }
+    
     return <section className="py-24 px-4 relative bg-secondary/30">
         <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Get In <span className="text-primary">Touch</span>
